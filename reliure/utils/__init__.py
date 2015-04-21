bc6e149ac261de5b3224a48b60805f8104603eff
Emmanuel N <enavarro222@gmail.com>
2015-04-21 12:33:49 +0200

    remove old urllib2 dependency (deprecated fct)

--- reliure/utils/__init__.py
+++ reliure/utils/__init__.py
@@ -11,7 +11,6 @@
 """
 
 import re
-import urllib2
 import logging
 import json
 
@@ -48,43 +47,6 @@ def parse_bool(value):
 JSON_CLEAN_LAST_COMMA = re.compile(",([\s]*\})")
 def _json_text_clean(json_text):
     return JSON_CLEAN_LAST_COMMA.sub(r"\1", json_text).strip()
-
-
-def urllib2_json_urlopen(request_url, request_data=None, logger=None):
-    """ Make a request with urllib2 and retrive a JSON (in unicode)
-    """
-    #TODO use requests
-    if isinstance(request_url, unicode):
-        request_url = request_url.encode('utf8')
-    
-    if logger is not None:
-        logger.debug("urllib2: open url = %s" % request_url)
-        logger.debug("urllib2: with params = %s" % request_data)
-    
-    response = urllib2.urlopen(request_url, request_data)
-    json_text = response.read()
-    json_text = _json_text_clean(json_text)
-    if isinstance(json_text, str):
-        json_text = json_text.decode('utf8')
-    # data are provided in json
-    try:
-        results = json.loads(json_text)
-    except ValueError:
-        logger.error("Fail to parse the json: %s" % json_text)
-        raise
-    response.close()
-    return results
-
-
-def urllib2_setup_proxy(proxy=None):
-    """ Setup a proxy for urllib2
-    """
-    # urllib2 tries to use the proxy to find the localhost
-    # Proxy support may not work at irit if no empty is set for the request
-    if proxy:
-        proxy_support = urllib2.ProxyHandler({'http' : proxy})
-        opener = urllib2.build_opener(proxy_support)
-        urllib2.install_opener(opener)
 
 
 def deprecated(new_fct_name, logger=None):
