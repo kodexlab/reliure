293ea45f93340dc3028fbeba901fe6415f99d9e3
Emmanuel N <enavarro222@gmail.com>
2016-11-23 19:17:47 +0100

    Ch for version 16.12

--- reliure/__init__.py
+++ reliure/__init__.py
@@ -2,7 +2,7 @@
 """:mod:`reliure`
 =================
 """
-__version__ = '15.10'
+__version__ = '16.12'
 __all__ = ['Composable', 'Optionable', 'Map']
 
 from reliure.pipeline import Composable, Optionable, Map
